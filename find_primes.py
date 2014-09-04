6191328d7003bedeb0db12b75482735273ab31c1
Kale Kundert <kale@thekunderts.net>
2014-09-04 16:23:32 -0700

    Implement the 'sieve of eratosthenes' algorithm.

--- find_primes.py
+++ find_primes.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 
-def find_primes(limit):
+from math import sqrt
+
+def trial_division(limit):
     primes = []
 
     for candidate in range(2, limit + 1):
         candidate_ok = True
 
-        for divisor in range(2, candidate):
+        for divisor in range(2, int(sqrt(candidate)) + 1):
             if candidate % divisor == 0:
                 candidate_ok = False
                 break
@@ -16,9 +18,37 @@ def find_primes(limit):
 
     return primes
 
+def sieve_of_eratosthenes(limit):
+    # Create an array containing a boolean value for every number between 2 and 
+    # the given limit.  The boolean value indicates whether or not that number 
+    # could be a prime.  At the beginning of the algorithm, every number could 
+    # be a prime (i.e. the array is True everywhere).  As the algorithm 
+    # progresses, more and more numbers will be eliminated.  By the end, only 
+    # primes will remain.
+
+    primes = []
+    sieve = [True for i in range(limit + 1)]
+
+    for candidate in range(2, limit + 1)
+        if sieve[candidate]:
+            primes.append(candidate)
+
+            for not_prime in range(2 * candidate, limit + 1, candidate):
+                sieve[not_prime] = False
+
+    return primes
+
 if __name__ == '__main__':
     import sys
-    limit = int(sys.argv[1])
-    print find_primes(limit)
+
+    algorithm = sys.argv[1]
+    limit = int(sys.argv[2])
+
+    if 'div' in algorithm:
+        print trial_division(limit)
+    elif 'erat' in algorithm:
+        print sieve_of_eratosthenes(limit)
+    else:
+        print 'Unknown algorithm: {}'.format(algorithm)
 
 
